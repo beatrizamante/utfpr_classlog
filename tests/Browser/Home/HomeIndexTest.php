b9e7952dfc280539435fec1c94c0bc81e48add87
João Paulo Abdala Bohaczk <okiabdala@gmail.com>
2024-11-19 21:48:05 -0300

    test fix

---
+++ tests/Browser/Home/HomeIndexTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Browser\Problems;
+
+use PHPUnit\Framework\TestCase as FrameworkTestCase;
+
+class HomeIndexTest extends FrameworkTestCase
+{
+    public function test_should_have_the_homepage(): void
+    {
+        $page = file_get_contents('http://web/');
+
+        $statusCode = $http_response_header[0];
+
+        $this->assertEquals('HTTP/1.1 200 OK', $statusCode);
+    }
+
+}
